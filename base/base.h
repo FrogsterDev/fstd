1b93fb9363d47e6e0a8e10f6d7a37fa88014a7f6
Zabsooon <jakub.zabski@protonmail.com>
2025-03-03 15:59:38 +0100

    Clean-up code for Compiler/OS/Architecture detection macros

--- base/base.h
+++ base/base.h
@@ -1,78 +1,42 @@
 #ifndef FSTD_BASE_H
 #define FSTD_BASE_H
 
-/* Compiler/OS/Architecture macros */
+/* Compiler Detection */
 #if defined(_MSC_VER)
-#   define COMPILER_MSVC 1
-#   if defined(_WIN32)
-#       define OS_WINDOWS 1
-#   else
-#       error OS not detected for COMPILER_MSVC
-#   endif
-#   if defined(_M_AMD64)
-#       define ARCH_X64 1
-#   elif defined(_M_IX86)
-// Note(Zabsooon): This is the architecture only defined for 32-bits
-#       define ARCH_X86 1
-#   elif defined(_M_ARM)
-#       define ARCH_ARM 1
-#   else
-#       error architecture not detected for COMPILER_MSVC
-#   endif
-
+    #define COMPILER_MSVC 1
 #elif defined(__clang__)
-#   define COMPILER_CLANG 1
-#   if defined(_WIN32)
-#       define OS_WINDOWS 1
-#   elif defined(__gnu_linux__)
-#       define OS_LINUX 1
-#   elif defined(__APPLE__) && defined(__MACH__)
-#       define OS_MAC 1
-#   else
-#       error OS not detected for COMPILER_CLANG
-#   endif
-#   if defined(__amd64__)
-#       define ARCH_X64 1
-#   elif defined(__i386__)
-#       define ARCH_X86 1
-#   elif defined(__arm__)
-#       define ARCH_ARM 1
-#   elif defined(__aarch64__)
-#       define ARCH_ARM64 1
-#   else
-#       error architecture not detected for COMPILER_CLANG
-#   endif
-
+    #define COMPILER_CLANG 1
 #elif defined(__GNUC__)
-#   define COMPILER_GCC 1
-#   if defined(_WIN32)
-#       define OS_WINDOWS 1
-#   elif defined(__gnu_linux__)
-#       define OS_LINUX 1
-#   elif defined(__APPLE__) && defined(__MACH__)
-#       define OS_MAC 1
-#   else
-#       error OS not detected for COMPILER_GCC
-#   endif
-#   if defined(__amd64__)
-#       define ARCH_X64 1
-#   elif defined(__i386__)
-#       define ARCH_X86 1
-#   elif defined(__arm__)
-#       define ARCH_ARM 1
-#   elif defined(__aarch64__)
-#       define ARCH_ARM64 1
-#   else
-#       error architecture not detected for COMPILER_GCC
-#   endif
-
-
+    #define COMPILER_GCC 1
 #else
-#   error no macros support for this compiler
+    #error "Unsupported compiler"
 #endif
 
+/* Os Detection */
+#if defined(_WIN32)
+    #define OS_WINDOWS 1
+#elif defined(__gnu_linux__)
+    #define OS_LINUX 1
+#elif defined(__APPLE__) && defined(__MACH__)
+    #define OS_MAC 1
+#else
+    #error "Unsupported OS"
+#endif
 
+/* Architecture Detection */
+#if defined(_M_AMD64) || defined(__amd64__) || defined(__x86_64__)
+    #define ARCH_X64 1
+#elif defined(_M_IX86) || defined(__i386__)
+    #define ARCH_X86 1
+#elif defined(_M_ARM) || defined(__arm__) || defined(__arm)
+    #define ARCH_ARM 1
+#elif defined(_M_ARM64) || defined(__aarch64__)
+    #define ARCH_ARM64 1
+#else
+    #error "Unsupported architecture"
+#endif
 
+/* Define if not defined */
 #if !defined(COMPILER_MSVC)
 #   define COMPILER_MSVC 0
 #endif
@@ -103,7 +67,5 @@
 #if !defined(ARCH_ARM64)
 #   define ARCH_ARM64 0
 #endif
-
-
 
 #endif // FSTD_BASE_H
