2ef76e591a3cea72c4b435a090c5391bbee427c2
Zabsooon <jakub.zabski@protonmail.com>
2025-03-05 18:30:40 +0100

    Add assert and own types

--- base/base.h
+++ base/base.h
@@ -2,6 +2,7 @@
 #define FSTD_BASE_H
 
 /* Compiler Detection */
+#include <cstdint>
 #if defined(_MSC_VER)
     #define COMPILER_MSVC 1
 #elif defined(__clang__)
@@ -38,34 +39,72 @@
 
 /* Define if not defined */
 #if !defined(COMPILER_MSVC)
-#   define COMPILER_MSVC 0
+    #define COMPILER_MSVC 0
 #endif
 #if !defined(COMPILER_CLANG)
-#   define COMPILER_CLANG 0
+    #define COMPILER_CLANG 0
 #endif
 #if !defined(COMPILER_GCC)
-#   define COMPILER_GCC 0
+    #define COMPILER_GCC 0
 #endif
 #if !defined(OS_WINDOWS)
-#   define OS_WINDOWS 0
+    #define OS_WINDOWS 0
 #endif
 #if !defined(OS_LINUX)
-#   define OS_LINUX 0
+    #define OS_LINUX 0
 #endif
 #if !defined(OS_MAC)
-#   define OS_MAC 0
+    #define OS_MAC 0
 #endif
 #if !defined(ARCH_X64)
-#   define ARCH_X64 0
+    #define ARCH_X64 0
 #endif
 #if !defined(ARCH_X86)
-#   define ARCH_X86 0
+    #define ARCH_X86 0
 #endif
 #if !defined(ARCH_ARM)
-#   define ARCH_ARM 0
+    #define ARCH_ARM 0
 #endif
 #if !defined(ARCH_ARM64)
-#   define ARCH_ARM64 0
+    #define ARCH_ARM64 0
 #endif
+
+
+/* Asserts works the other way around than the <cassert>
+ * in C asserts are enabled by default, and can be dissabled by declaring NDEBUG
+ * here the assert have to be enabled explicitly */
+#if ENABLE_ASSERT
+    #define Assert(x) do { if(!(c)) { (*(int*)0) = 0 } } while(0);
+#elif !defined(ENABLE_ASSERT)
+    #define Assert(x)
+#endif
+
+/* Types */
+#include <stdint.h>
+typedef int8_t      i8;
+typedef int16_t     i16;
+typedef int32_t     i32;
+typedef int64_t     i64;
+typedef uint8_t     ui8;
+typedef uint16_t    ui16;
+typedef uint32_t    ui32;
+typedef uint64_t    ui64;
+typedef float       f32;
+typedef double      f64;
+
+static i8 min_i8 = (i8)0x80;
+static i16 min_i16 = (i16)0x800;
+static i32 min_i32 = (i32)0x80000000;
+static i64 min_i64 = (i64)0x8000000000000000llu;
+
+static i8 max_i8 = (i8)0x7f;
+static i16 max_i16 = (i16)0x7fff;
+static i32 max_i32 = (i32)0x7fffffff;
+static i64 max_i64 = (i64)0x7fffffffffffffffllu;
+
+static ui8 max_ui8 = (ui8)0xff;
+static ui16 max_ui16 = (ui16)0xffff;
+static ui32 max_ui32 = (ui32)0xffffffff;
+static ui64 max_ui64 = (ui64)0xffffffffffffffffllu;
 
 #endif // FSTD_BASE_H
